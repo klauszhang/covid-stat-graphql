2da3c5a1fe9fec01a2aaa25351db01d7153c6ce5
hunter-syndex <hunter.geng@syndex.exchange>
2022-01-21 22:28:22 +1300

    feat: add more country resolvers

--- src/resolvers/country.js
+++ src/resolvers/country.js
@@ -1,15 +1,70 @@
+/*-------------- Query start -------------- */
+
 async function countries(_, __, { dataSources }) {
   const { lmao } = dataSources
   return lmao.getCountries()
 }
 
-function country() {
-  throw new Error('not implemented')
+async function country(_, args, { dataSources }) {
+  const { lmao } = dataSources
+  const countries = await lmao.getCountries()
+  return countries?.find(
+    (item) => item.country.toLowerCase() === args.name.toLowerCase()
+  )
 }
+
+async function top10MostAffectedCountries(_, __, { dataSources }) {
+  const { lmao } = dataSources
+  const countries = await lmao.getCountries()
+  return countries
+    ?.sort((itemA, itemB) => itemB.cases - itemA.cases)
+    .slice(0, 10)
+}
+
+async function totalCases(_, __, { dataSources }) {
+  const { lmao } = dataSources
+  const countries = await lmao.getCountries()
+  return countries?.reduce((pre, cur) => pre + cur.cases, 0)
+}
+
+/*-------------- Query end -------------- */
+
+/*-------------- Mutation start -------------- */
+
+async function deleteCountry(_, args, { dataSources }) {
+  const { lmao } = dataSources
+  const countries = await lmao.getCountries()
+  return countries?.filter(
+    (item) => item.country.toLowerCase() !== args.name.toLowerCase()
+  )
+}
+
+async function updateCountryCases(_, args, { dataSources }) {
+  const { lmao } = dataSources
+  const countries = await lmao.getCountries()
+  const index = countries?.findIndex(
+    (item) => item.country.toLowerCase() === args.name.toLowerCase()
+  )
+
+  if (index === -1) {
+    return new Error(`Cannot find country: ${args.name}`)
+  }
+
+  countries[index].cases = args.cases
+  return countries
+}
+
+/*-------------- Mutation end -------------- */
 
 module.exports = {
   Query: {
     countries,
     country,
+    top10MostAffectedCountries,
+    totalCases,
+  },
+  Mutation: {
+    deleteCountry,
+    updateCountryCases,
   },
 }
--- src/server.js
+++ src/server.js
@@ -12,6 +12,16 @@ const resolvers = loadFilesSync(path.join(__dirname, '/resolvers'))
 const server = new ApolloServer({
   typeDefs: mergeTypeDefs(schemas),
   resolvers: mergeResolvers(resolvers),
+  introspection: process.env.NODE_ENV !== 'production',
+  formatError(err) {
+    // Don't give the specific errors to the client.
+    if (err.message.startsWith('Database Error: ')) {
+      return new Error('Internal server error')
+    }
+    // Otherwise return the original error. The error can also
+    // be manipulated in other ways, as long as it's returned.
+    return err
+  },
   dataSources: () => {
     return {
       lmao: lmaoAPI,
