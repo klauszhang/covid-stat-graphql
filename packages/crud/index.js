65cec2075d9ea709078bf26d0efacd342e00fce6
Oly <opfhyde@gmail.com>
2022-03-16 18:01:10 +1300

    using inbuilt json parser

--- packages/crud/index.js
+++ packages/crud/index.js
@@ -1,70 +1,70 @@
-var express = require("express");
-var app = express();
-var PORT = 3000;
-var bodyParser = require("body-parser");
+var express = require('express')
+var app = express()
+var PORT = 3000
 
-var books = require("./mocks/books");
-var authors = require("./mocks/authors");
+var books = require('./mocks/books')
+var authors = require('./mocks/authors')
 
-app.use(bodyParser.json());
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded())
 
-app.get("/books", (req, res) => {
-  res.json(books);
-});
+app.get('/books', (req, res) => {
+  res.json(books)
+})
 
-app.get("/book/:id", (req, res) => {
-  res.json(books.find((book) => book.id === parseInt(req.params.id)));
-});
+app.get('/book/:id', (req, res) => {
+  res.json(books.find((book) => book.id === parseInt(req.params.id)))
+})
 
-app.get("/author/:id", (req, res) => {
-  res.json(authors.find((author) => author.id === parseInt(req.params.id)));
-});
+app.get('/author/:id', (req, res) => {
+  res.json(authors.find((author) => author.id === parseInt(req.params.id)))
+})
 
-app.get("/authorBooks/:id", (req, res) => {
-  res.json(books.filter((book) => book.author_id === parseInt(req.params.id)));
-});
+app.get('/authorBooks/:id', (req, res) => {
+  res.json(books.filter((book) => book.author_id === parseInt(req.params.id)))
+})
 
-app.post("/book", function (req, res) {
+app.post('/book', function (req, res) {
   if (!req.body.title || !req.body.author_id) {
-    res.status(400);
-    res.send("None shall pass");
-    return;
+    res.status(400)
+    res.send('None shall pass')
+    return
   }
 
-  const newBook = { id: books.length + 1, ...req.body };
-  books.push(newBook);
-  res.json(newBook);
-});
+  const newBook = { id: books.length + 1, ...req.body }
+  books.push(newBook)
+  res.json(newBook)
+})
 
-app.delete("/book/:id", function (req, res) {
+app.delete('/book/:id', function (req, res) {
   if (!req.params.id) {
-    res.status(400);
-    res.send("None shall pass");
-    return;
+    res.status(400)
+    res.send('None shall pass')
+    return
   }
 
-  books = books.filter((book) => book.id !== parseInt(req.params.id));
-  res.json(books);
-});
+  books = books.filter((book) => book.id !== parseInt(req.params.id))
+  res.json(books)
+})
 
-app.put("/book/:id", function (req, res) {
+app.put('/book/:id', function (req, res) {
   if (!req.params.id || !req.body.title || !req.body.author_id) {
-    res.status(400);
-    res.send("None shall pass");
-    return;
+    res.status(400)
+    res.send('None shall pass')
+    return
   }
 
   books = books.map((book) => {
-    if (book.id !== parseInt(req.params.id)) return book;
+    if (book.id !== parseInt(req.params.id)) return book
     return {
       id: book.id,
       ...req.body,
-    };
-  });
-  res.json(books);
-});
+    }
+  })
+  res.json(books)
+})
 
 app.listen(PORT, function (err) {
-  if (err) console.log(err);
-  console.log("Server listening on PORT", PORT);
-});
+  if (err) console.log(err)
+  console.log('Server listening on PORT', PORT)
+})
