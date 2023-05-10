df824cfce11125f23062d0382bbbe690a9de0a21
Kevin Oh <hanmuk.oh@invenco.com>
2023-05-10 23:07:53 +1200

    Remove redundant code

--- src/server.ts
+++ src/server.ts
@@ -2,10 +2,7 @@ import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
 
 import LmaoAPI from './services/lmao'
-// import resolvers from './resolvers'
-// import schemas from './schemas'
 import { loadFilesSync } from '@graphql-tools/load-files'
-import { mergeTypeDefs, mergeResolvers } from '@graphql-tools/merge'
 import path from 'path'
 
 const port = 3001
